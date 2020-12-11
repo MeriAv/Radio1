14575d0f567c39d1a38ff5b9b682060b5b315bab
MeriAv <mari.avdeeva1407@gmal.com>
2020-12-11 15:21:05 +0300

    set

--- src/test/java/ru/netilogi/domane/TestRadio.java
+++ src/test/java/ru/netilogi/domane/TestRadio.java
@@ -12,7 +12,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonNextStationOver() {
         Radio radio = new Radio();
-        int currentStation = 11;
+        radio.setCurrentStation (11);
         int expected = 0;
         int actual = radio.buttonNextStation();
         assertEquals(expected, actual);
@@ -22,7 +22,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonNextStationNorm() {
         Radio radio = new Radio();
-        int currentStation = 1;
+        radio.setCurrentStation(1);
         int expected = 2;
         int actual = radio.buttonNextStation();
         assertEquals(expected, actual);
@@ -32,7 +32,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonPrefStationNorm() {
         Radio radio = new Radio();
-        int currentStation = 5;
+        radio.setCurrentStation(5);
         int expected = 4;
         int actual = radio.buttonPrefStation();
         assertEquals(actual, expected);
@@ -41,7 +41,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonPrefStationMin() {
         Radio radio = new Radio();
-        int currentStation = 0;
+        radio.setCurrentStation(0);
         int expected = 9;
         int actual = radio.buttonPrefStation();
         assertEquals(actual, expected);
@@ -50,7 +50,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonNextVolumeNorm() {
         Radio radio = new Radio();
-        int currentVolume = 5;
+        radio.setCurrentVolume(5);
         int actual = 6;
         int expected = radio.buttonNextVolume();
         assertEquals(actual, expected);
@@ -59,7 +59,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonNextVolumeOver() {
         Radio radio = new Radio();
-        int currentVolume = 10;
+        radio.setCurrentVolume(10);
         int actual = 10;
         int expected = radio.buttonNextVolume();
         assertEquals(actual, expected);
@@ -69,7 +69,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonPrefVolumeMin() {
         Radio radio = new Radio();
-        int currentVolume = 0;
+        radio.setCurrentVolume(0);
         int actual = 0;
         int expected = radio.buttonPrefVolume();
         assertEquals(actual, expected);
@@ -78,7 +78,7 @@ public class TestRadio {
     @Test
     public void snouldRadioButtonPrefVolumeNorm() {
         Radio radio = new Radio();
-        int currentVolume = 4;
+        radio.setCurrentVolume(4);
         int actual = 3;
         int expected = radio.buttonPrefVolume();
         assertEquals(actual, expected);
